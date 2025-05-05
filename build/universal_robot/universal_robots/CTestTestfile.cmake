e969c7106a3ab66d165651210783c38f8d00b1e9
Asif Ali <mguasifali@gmail.com>
2025-05-05 15:31:53 +0530

    Initial commit

---
+++ build/universal_robot/universal_robots/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/asifali/xrobo_ws/src/universal_robot/universal_robots
+# Build directory: /home/asifali/xrobo_ws/build/universal_robot/universal_robots
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
